659526d237244ecd9c3a3948ad6c5687e4852e02
SHREYASI DAS <dnilanjan18@gmail.com>
2024-03-11 21:58:31 +0530

    Add files via upload

---
+++ Interns/ShreyasiDas/Test/swap.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;  
+class swap {  
+    public static void main(String[] args) {  
+       int x, y, t;   
+       Scanner sc = new Scanner(System.in);  
+       System.out.println("Enter the value of X and Y");  
+       x = sc.nextInt();  
+       y = sc.nextInt();  
+       System.out.println("before swapping numbers: "+x +"  "+ y);  
+       
+       t = x;  
+       x = y;  
+       y = t;  
+       System.out.println("After swapping: "+x +"   " + y);  
+       System.out.println( );  
+       sc.close(); 
+    }    
+}  
